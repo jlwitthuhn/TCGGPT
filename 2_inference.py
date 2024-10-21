7a107638b209d1f2f4b1828e65595721d9dbff65
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2024-10-20 22:00:48 -0700

    Update inference script to use argparse

--- 2_inference.py
+++ 2_inference.py
@@ -1,26 +1,25 @@
+import argparse
 import sys
 
 from cardgen.card_model import CardModel
 from cardgen.tokenizer import CardTokenizer
 
 
-def print_help():
-    print(
-        "Usage: python3 2_inference.py [path to model.safetensors] [generation count]"
-    )
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print_help()
-        sys.exit(0)
+    arg_parser = argparse.ArgumentParser(
+        description="Run inference for a cardgen model",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    arg_parser.add_argument("model_path", help="Path to model safetensors file")
+    arg_parser.add_argument("--count", default=3, type=int, help="Number of cards to generate")
+    args = arg_parser.parse_args()
 
-    model_path: str = sys.argv[1]
+    model_path: str = args.model_path
     model: CardModel = CardModel.load_file(model_path)
     tokenizer_path = model_path.replace(".safetensors", ".tokenizer")
     tokenizer: CardTokenizer = CardTokenizer.load_file(tokenizer_path)
 
-    for _ in range(int(sys.argv[2])):
+    for _ in range(args.count):
         start = tokenizer.encode_token("<NewCard>")
         output = model.generate_card(start)
         output_strs: list[str] = []

e248f8312fa2cc73314678a514e5b39ba40951f9
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-08-23 18:48:54 -0700

    Decompose clean into multiple files pt 3

--- cardgen/clean/__init__.py
+++ cardgen/clean/__init__.py
@@ -1,6 +1,6 @@
 import re
 
-from .dynamic import clean_named_cards
+from .dynamic import clean_named_cards, clean_plural_types
 from .simple import clean_basic
 from .strings import (
     FLAVOR_ABILITY_WORD,
@@ -328,19 +328,7 @@ VERBS_LIST.sort(key=len, reverse=True)
 
 def _clean_special_words(the_card, plural_type_map: dict[str, str]):
 
-    for word in plural_type_map:
-        if word in the_card["oracle_text"]:
-            # Do not clean this word if it is immediately followed by a letter
-            # This is needed to stop the keyword 'demonstrate' from being treated like 'demons' and similar
-            next_index = the_card["oracle_text"].find(word) + len(word)
-            if (
-                next_index < len(the_card["oracle_text"])
-                and the_card["oracle_text"][next_index].isalpha()
-            ):
-                continue
-            the_card["oracle_text"] = the_card["oracle_text"].replace(
-                word, plural_type_map[word]
-            )
+    clean_plural_types(the_card, plural_type_map)
 
     for word in PLURALS:
         if word in the_card["oracle_text"]:
--- cardgen/clean/dynamic.py
+++ cardgen/clean/dynamic.py
@@ -1,5 +1,7 @@
 # Cleaning algorithms that depend on data dynamically gathered from the dataset
 
+from .strings import NAMED_CARD
+
 
 def clean_named_cards(the_card, name_set: set[str]):
     # In reverse order of length so we always replace the longer strings first
@@ -9,4 +11,20 @@ def clean_named_cards(the_card, name_set: set[str]):
         if this_name in the_card["oracle_text"]:
             the_card["oracle_text"] = the_card["oracle_text"].replace(
                 this_name, NAMED_CARD
+            )
+
+
+def clean_plural_types(the_card, plural_type_map: dict[str, str]):
+    for word in plural_type_map:
+        if word in the_card["oracle_text"]:
+            # Do not clean this word if it is immediately followed by a letter
+            # This is needed to stop the keyword 'demonstrate' from being treated like 'demons' and similar
+            next_index = the_card["oracle_text"].find(word) + len(word)
+            if (
+                next_index < len(the_card["oracle_text"])
+                and the_card["oracle_text"][next_index].isalpha()
+            ):
+                continue
+            the_card["oracle_text"] = the_card["oracle_text"].replace(
+                word, plural_type_map[word]
             )
